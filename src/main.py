8b85fd2576906914c510ddda831e351b48bf5f73
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-05-12 16:55:57 -0700

    Logging

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import logging
+from argparse import ArgumentParser
+
+from utils.logger import create_logger, set_all_stdout_logger_levels
+
+logger = create_logger(name=__name__, level=logging.INFO)
+
+parser = ArgumentParser(description="Convert your MakeCode Arcade games into a "
+                                    "standalone offline executable!")
+parser.add_argument("--debug", action="store_true",
+                    help="Enable debug logging.")
+args = parser.parse_args()
+debug = bool(args.debug)
+if debug:
+    set_all_stdout_logger_levels(logging.DEBUG)
+logger.debug(f"Received arguments: {args}")
